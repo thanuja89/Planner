46447b42066c17697a5de2811eb7c334cc027cb4
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-01-26 21:30:49 +0530

    A test added.

--- Planner.Api.Tests/ScheduledTaskControllerTests.cs
+++ Planner.Api.Tests/ScheduledTaskControllerTests.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Planner.Api.Controllers;
-using Planner.Domain.Entities;
-using Planner.Domain.Repositories;
-using System;
+using Planner.Domain.Repositories.Interfaces;
 using System.Security.Claims;
 using Xunit;
 
@@ -13,27 +11,45 @@ namespace Planner.Api.Tests
 {
     public class ScheduledTaskControllerTests
     {
-        private readonly string _userId;
-        private Mock<ScheduledTaskRepository> _mockRepo;
+        private string _userId;
+        private Mock<IScheduledTaskRepository> _mockRepo;
         private Mock<IMapper> _mockMapper;
         private ScheduledTaskController _sut;
 
-        public ScheduledTaskControllerTests()
+        [Fact]
+        public async void Get_ValidPrinciple_CallsRepoWithCorrectArgsAndReturnsOk()
+        {
+            // Arrange
+            SetUp();
+
+            // Act
+            var result = await _sut.Get();
+
+            // Assert
+            _mockRepo.Verify(m => m.GetScheduledTasksForUser(_userId));
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        private void SetUp()
         {
             _userId = "abcadasda";
 
-            var user = new ClaimsPrincipal();
+            _mockRepo = new Mock<IScheduledTaskRepository>();
+            _mockMapper = new Mock<IMapper>();
 
             var claimsPrinc = new ClaimsPrincipal(new ClaimsIdentity(
                 new Claim[] { new Claim(ClaimTypes.NameIdentifier, _userId) }));
 
-            _sut.HttpContext.User = user;
-        }
-
-        [Fact]
-        public void Get_ValidPrinciple_CallsRepository()
-        {
-
+            _sut = new ScheduledTaskController(_mockRepo.Object, _mockMapper.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        User = claimsPrinc
+                    }
+                }
+            };
         }
     }
 }
