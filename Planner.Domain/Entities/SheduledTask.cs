97fb7e6e360b61dccf4a998ee80ed5411436c692
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-01-26 13:58:48 +0530

    Db added.

---
+++ Planner.Domain/Entities/SheduledTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Planner.Domain.Entities
+{
+    public class SheduledTask
+    {
+        public long Id { get; set; }
+
+        [Column(TypeName = "VARCHAR(255)")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string Note { get; set; }
+        public Importance Importance { get; set; }
+        public Frequency Repeat { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool IsNotify { get; set; }
+        public bool IsAlarm { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ Planner.Domain/PlannerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Planner.Domain.Entities;
+
+namespace Planner.Domain
+{
+    public class PlannerDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<SheduledTask> SheduledTasks { get; set; }
+
+        public PlannerDbContext(DbContextOptions<PlannerDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
