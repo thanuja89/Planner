49e5b9ce3498ba0d5329c3112063e3ef262ae035
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-08 20:55:54 +0530

    Scheduled Task service added.

--- Planner.Mobile.Core/Services/HttpService.cs
+++ Planner.Mobile.Core/Services/HttpService.cs
@@ -15,23 +15,33 @@ namespace Planner.Mobile.Core.Services
             _httpClient = new HttpClient();
         }
 
-        public async Task<T> PostForResultAsync<T>(string url, object obj)
+        public async Task<T> GetAsync<T>(string url)
         {
-            var response = await Post(url, obj);
-
-            T result = default;
+            var response = await _httpClient.GetAsync($"{ CommonUrls.BASE_URI }{ url}");
 
             if (response.IsSuccessStatusCode)
             {
                 return await DeserializeResponseAsync<T>(response);
             }
 
-            return result;
+            return default;
+        }
+
+        public async Task<T> PostForResultAsync<T>(string url, object obj)
+        {
+            var response = await PostAsync(url, obj);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await DeserializeResponseAsync<T>(response);
+            }
+
+            return default;
         }
 
         public async Task<T> PostForResultAsync<T>(string url, object obj, bool isDeserializeUnsuccesful)
         {
-            var response = await Post(url, obj);
+            var response = await PostAsync(url, obj);
 
             if (isDeserializeUnsuccesful)
                 return await DeserializeResponseAsync<T>(response);
@@ -46,7 +56,7 @@ namespace Planner.Mobile.Core.Services
 
         public async Task<bool> PostForSuccessResultAsync(string url, object obj)
         {
-            var response = await Post(url, obj);
+            var response = await PostAsync(url, obj);
 
             if (response.StatusCode == HttpStatusCode.OK)
                 return true;
@@ -54,12 +64,37 @@ namespace Planner.Mobile.Core.Services
             return false;
         }
 
-        public Task<HttpResponseMessage> Post(string url, object obj)
+        public Task<HttpResponseMessage> PostAsync(string url, object obj)
         {
             var json = JsonConvert.SerializeObject(obj);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             return _httpClient.PostAsync($"{ CommonUrls.BASE_URI }{ url}", content);
+        }
+
+        public Task<HttpResponseMessage> PutAsync(string url, object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return _httpClient.PutAsync($"{ CommonUrls.BASE_URI }{ url}", content);
+        }
+
+        public async Task<T> PutForResultAsync<T>(string url, object obj)
+        {
+            var response = await PutAsync(url, obj);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await DeserializeResponseAsync<T>(response);
+            }
+
+            return default;
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            return _httpClient.DeleteAsync($"{ CommonUrls.BASE_URI }{ url}");
         }
 
         private async Task<T> DeserializeResponseAsync<T>(HttpResponseMessage httpResponse)
---
+++ Planner.Mobile.Core/Services/ScheduledTaskService.cs
@@ -0,0 +1,41 @@
+﻿using Planner.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Planner.Mobile.Core.Services
+{
+    public class ScheduledTaskService
+    {
+        private readonly HttpService _httpService;
+
+        public ScheduledTaskService()
+        {
+            _httpService = new HttpService();
+        }
+
+        public Task<IEnumerable<GetScheduledTaskDTO>> GetScheduledTasksAsync()
+        {
+            return _httpService.GetAsync<IEnumerable<GetScheduledTaskDTO>>("ScheduledTask");
+        }
+
+        public Task<IEnumerable<GetScheduledTaskDTO>> GetScheduledTaskAsync(int id)
+        {
+            return _httpService.GetAsync<IEnumerable<GetScheduledTaskDTO>>($"ScheduledTask/{ id }");
+        }
+
+        public Task<GetScheduledTaskDTO> CreateScheduledTaskAsync(ScheduledTaskDTO taskDTO)
+        {
+            return _httpService.PostForResultAsync<GetScheduledTaskDTO>("ScheduledTask", taskDTO);
+        }
+
+        public Task<GetScheduledTaskDTO> UpdateScheduledTaskAsync(ScheduledTaskDTO taskDTO)
+        {
+            return _httpService.PutForResultAsync<GetScheduledTaskDTO>("ScheduledTask", taskDTO);
+        }
+
+        public Task DeleteScheduledTaskAsync(int id)
+        {
+            return _httpService.DeleteAsync("ScheduledTask");
+        }
+    }
+}
