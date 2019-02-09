6bbfe136aa38635038a0ef51a4ec90c7d1877fdd
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-09 08:26:15 +0530

    Planner db added.

---
+++ Planner.Mobile.Core/Services/ScheduledTaskDataService.cs
@@ -0,0 +1,29 @@
+﻿using Planner.Mobile.Core.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Planner.Mobile.Core.Services
+{
+    public class ScheduledTaskDataService
+    {
+        public Task<List<ScheduledTask>> GetScheduledTasksAsync()
+        {
+            return PlannerDatabase.Instance
+                .GetAll<ScheduledTask>()
+                .ToListAsync();
+        }
+
+        public Task<ScheduledTask> GetScheduledTaskAsync(int id)
+        {
+            return PlannerDatabase.Instance
+                .GetAll<ScheduledTask>()
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public Task InsertScheduledTaskAsync(ScheduledTask task)
+        {
+            return PlannerDatabase.Instance
+                .InsertAsync(task);
+        }
+    }
+}
