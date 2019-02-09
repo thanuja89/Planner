1221ba7f1d53b1d4d5cdf51073dbeb8ec43a53f8
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-09 12:01:00 +0530

    Filter tasks by date range methods added.

--- Planner.Mobile.Core/Data/PlannerDatabase.cs
+++ Planner.Mobile.Core/Data/PlannerDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using SQLite;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -35,9 +36,24 @@ namespace Planner.Mobile.Core.Data
             return _connection.Table<T>();
         }
 
+        public Task<List<T>> QueryAll<T>(string query, params object[] objs) where T : new()
+        {
+            return _connection.QueryAsync<T>(query, objs);
+        }
+
         public Task InsertAsync<T>(T item) where T : new()
         {
             return _connection.InsertAsync(item);
+        }
+
+        public Task UpdateAsync<T>(T item) where T : new()
+        {
+            return _connection.UpdateAsync(item);
+        }
+
+        public Task DeleteAsync<T>(T item) where T : new()
+        {
+            return _connection.DeleteAsync(item);
         }
 
         private void InitDatabase()
