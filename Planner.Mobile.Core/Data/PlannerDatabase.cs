c684dc20f7d3ef0acd33778ddf7c959a51b52f49
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-09 08:26:15 +0530

    Planner db added.

---
+++ Planner.Mobile.Core/Data/PlannerDatabase.cs
@@ -0,0 +1,48 @@
+﻿using SQLite;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Planner.Mobile.Core.Data
+{
+    public class PlannerDatabase
+    {
+        private static PlannerDatabase database;
+        private readonly SQLiteAsyncConnection _connection;
+
+        public PlannerDatabase(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+
+            InitDatabase();
+        }
+
+        public static PlannerDatabase Instance
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new PlannerDatabase(
+                      Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PlannerSQLite.db3"));
+                }
+                return database;
+            }
+        }
+
+        public AsyncTableQuery<T> GetAll<T>() where T : new ()
+        {
+            return _connection.Table<T>();
+        }
+
+        public Task InsertAsync<T>(T item) where T : new()
+        {
+            return _connection.InsertAsync(item);
+        }
+
+        private void InitDatabase()
+        {
+            _connection.CreateTableAsync<ScheduledTask>().Wait();
+        }
+    }
+}
---
+++ Planner.Mobile.Core/Data/ScheduledTask.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using System;
+
+namespace Planner.Mobile.Core.Data
+{
+    public class ScheduledTask
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string Note { get; set; }
+        public Importance Importance { get; set; }
+        public Frequency Repeat { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool IsNotify { get; set; }
+        public bool IsAlarm { get; set; }
+    }
+
+    public enum Importance
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum Frequency
+    {
+        Never,
+        EveryDay,
+        Weekdays,
+        Weekends,
+        Weekly,
+        Monthly,
+        Yearly
+    }
+}
