cb558c261855ef99bccb3ded621652512c27a494
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-16 16:04:13 +0530

    Debugged.

--- Planner.Droid/CreateTaskActivity.cs
+++ Planner.Droid/CreateTaskActivity.cs
@@ -1,26 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using Planner.Droid.Extensions;
+using Planner.Droid.Fragments;
+using Planner.Mobile.Core.Data;
+using Planner.Mobile.Core.Services;
+using System;
 
 namespace Planner.Droid
 {
-    [Activity(Label = "CreateTaskActivity", MainLauncher = true)]
+    //[Activity(Label = "CreateTaskActivity", MainLauncher = true)]
     public class CreateTaskActivity : Activity
     {
-        private EditText _titleEditText;
-        private EditText _descriptionEditText;
-        private Spinner _importanceSpinner;
-        private Spinner _repeatSpinner;
-        private Button _addNoteButton;
-        private Button _saveButton;
+        private EditText titleEditText;
+        private EditText descriptionEditText;
+        private Spinner importanceSpinner;
+        private Spinner repeatSpinner;
+        private Button addNoteButton;
+        private Button saveButton;
+        private Button startDateButton;
+        private Button endDateButton;
+        private TextView startDateText;
+        private TextView endDateText;
+        private readonly ScheduledTaskDataService _taskDataService;
+
+        public CreateTaskActivity()
+        {
+            _taskDataService = new ScheduledTaskDataService();
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,6 +38,8 @@ namespace Planner.Droid
             FindViews();
 
             SetUpSpinners();
+
+            HandleEvents();
         }
 
         private void SetUpSpinners()
@@ -39,7 +48,7 @@ namespace Planner.Droid
                 .GetNames(typeof(Mobile.Core.Data.Importance));
 
             var repeatItems = Enum
-                .GetNames(typeof(Mobile.Core.Data.Frequency));
+                .GetNames(typeof(Frequency));
 
             var importanceAdapter = new ArrayAdapter<string>(this,
                 Android.Resource.Layout.SimpleSpinnerItem, importanceItems);
@@ -47,28 +56,96 @@ namespace Planner.Droid
             var repeatAdapter = new ArrayAdapter<string>(this,
                 Android.Resource.Layout.SimpleSpinnerItem, repeatItems);
 
-            _importanceSpinner.Adapter = importanceAdapter;
-            _repeatSpinner.Adapter = repeatAdapter;
+            importanceSpinner.Adapter = importanceAdapter;
+            repeatSpinner.Adapter = repeatAdapter;
         }
 
         public void FindViews()
         {
-            _titleEditText = FindViewById<EditText>(Resource.Id.createTask_TitleEditText);
-            _descriptionEditText = FindViewById<EditText>(Resource.Id.createTask_DescriptionEditText);
-            _importanceSpinner = FindViewById<Spinner>(Resource.Id.createTask_ImportanceSpinner);
-            _repeatSpinner = FindViewById<Spinner>(Resource.Id.createTask_RepeatSpinner);
-            _addNoteButton = FindViewById<Button>(Resource.Id.createTask_AddNoteButton);
-            _saveButton = FindViewById<Button>(Resource.Id.createTask_SaveButton);
+            titleEditText = FindViewById<EditText>(Resource.Id.createTask_TitleEditText);
+            descriptionEditText = FindViewById<EditText>(Resource.Id.createTask_DescriptionEditText);
+            importanceSpinner = FindViewById<Spinner>(Resource.Id.createTask_ImportanceSpinner);
+            repeatSpinner = FindViewById<Spinner>(Resource.Id.createTask_RepeatSpinner);
+            addNoteButton = FindViewById<Button>(Resource.Id.createTask_AddNoteButton);
+            saveButton = FindViewById<Button>(Resource.Id.createTask_SaveButton);
+            startDateButton = FindViewById<Button>(Resource.Id.createTask_StartDateButton);
+            endDateButton = FindViewById<Button>(Resource.Id.createTask_EndDateButton);
+            startDateText = FindViewById<TextView>(Resource.Id.createTask_StartDateText);
+            endDateText = FindViewById<TextView>(Resource.Id.createTask_EndDateText);
         }
 
         private void HandleEvents()
         {
-            _saveButton.Click += _saveButton_Click;
+            startDateButton.Click += _startDateButton_Click;
+            endDateButton.Click += _endDateButton_Click;
+            saveButton.Click += _saveButton_Click;
         }
 
-        private void _saveButton_Click(object sender, EventArgs e)
+        private void _endDateButton_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            DatePickerFragment frag = DatePickerFragment.NewInstance((time) =>
+            {
+                endDateText.Text = time.ToLongDateString();
+            });
+
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
+        }
+
+        private void _startDateButton_Click(object sender, EventArgs e)
+        {
+            DatePickerFragment frag = DatePickerFragment.NewInstance((time) =>
+            {
+                startDateText.Text = time.ToLongDateString();
+            });
+
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
+        }
+
+        private async void _saveButton_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            var task = new ScheduledTask()
+            {
+                Title = titleEditText.Text,
+                Description = descriptionEditText.Text,
+                Start = DateTime.Parse(startDateText.Text),
+                End = DateTime.Parse(endDateText.Text),
+                Importance = (Mobile.Core.Data.Importance) importanceSpinner.SelectedItemPosition,
+                Repeat = (Frequency) repeatSpinner.SelectedItemPosition
+            };
+
+            await _taskDataService.InsertAsync(task);
+        }
+
+        private bool ValidateInputs()
+        {
+            if (titleEditText.IsEmpty())
+            {
+                titleEditText.Error = "Title can not be empty.";
+                return false;
+            }
+
+            if (descriptionEditText.IsEmpty())
+            {
+                descriptionEditText.Error = "Description can not be empty.";
+                return false;
+            }
+
+            if (startDateText.IsEmpty())
+            {
+                startDateText.Error = "Start Date can not be empty.";
+                return false;
+            }
+
+            if (endDateText.IsEmpty())
+            {
+                endDateText.Error = "End Date can not be empty.";
+                return false;
+            }
+
+            return true;
         }
     }
 }
