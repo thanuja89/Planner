ac0906135f8929256c92656e56827c5261ec678c
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-17 12:19:55 +0530

    Boolean properties set up.

--- Planner.Droid/CreateTaskActivity.cs
+++ Planner.Droid/CreateTaskActivity.cs
@@ -20,8 +20,38 @@ namespace Planner.Droid
         private TableRow endDateRow;
         private TextView startDateText;
         private TextView endDateText;
+        private ImageButton alarmButton;
+        private ImageButton notifyButton;
+        private RadioGroup importanceRadioGroup;
         private Button saveButton;
+
+        private bool _isAlarm;
+        private bool _isNotify;
+
         private readonly ScheduledTaskDataService _taskDataService;
+
+
+        public Mobile.Core.Data.Importance SelectedImportance
+        {
+            get
+            {
+                if (importanceRadioGroup == null)
+                    return Mobile.Core.Data.Importance.Medium;
+
+                switch (importanceRadioGroup.CheckedRadioButtonId)
+                {
+                    case Resource.Id.createTask_ImportanceRadio_Low:
+                        return Mobile.Core.Data.Importance.Low;
+
+                    case Resource.Id.createTask_ImportanceRadio_High:
+                        return Mobile.Core.Data.Importance.High;
+
+                    default:
+                        return Mobile.Core.Data.Importance.Medium;
+                }
+            }
+        }
+
 
         public CreateTaskActivity()
         {
@@ -48,6 +78,9 @@ namespace Planner.Droid
             endDateRow = FindViewById<TableRow>(Resource.Id.createTask_EndDateRow);
             startDateText = FindViewById<TextView>(Resource.Id.createTask_StartDateText);
             endDateText = FindViewById<TextView>(Resource.Id.createTask_EndDateText);
+            alarmButton = FindViewById<ImageButton>(Resource.Id.createTask_AlarmButton);
+            notifyButton = FindViewById<ImageButton>(Resource.Id.createTask_NotifyButton);
+            importanceRadioGroup = FindViewById<RadioGroup>(Resource.Id.createTask_ImportanceRadioGroup);
             saveButton = FindViewById<Button>(Resource.Id.createTask_SaveButton);
         }
 
@@ -55,7 +88,19 @@ namespace Planner.Droid
         {
             startDateRow.Click += _startDateButton_Click;
             endDateRow.Click += _endDateButton_Click;
+            alarmButton.Click += AlarmButton_Click;
+            notifyButton.Click += AlertButton_Click;
             saveButton.Click += _saveButton_Click;
+        }
+
+        private void AlertButton_Click(object sender, EventArgs e)
+        {
+            _isNotify = !_isNotify;
+        }
+
+        private void AlarmButton_Click(object sender, EventArgs e)
+        {
+            _isAlarm = !_isAlarm;
         }
 
         private void _endDateButton_Click(object sender, EventArgs e)
@@ -90,6 +135,10 @@ namespace Planner.Droid
                 Description = descriptionEditText.Text,
                 Start = DateTime.Parse(startDateText.Text),
                 End = DateTime.Parse(endDateText.Text),
+                IsAlarm = _isAlarm,
+                IsNotify = _isNotify,
+                Importance = SelectedImportance,
+                Note = noteEditText.Text
             };
 
             await _taskDataService.InsertAsync(task);
