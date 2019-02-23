d9e95deece6a3363218f1c91d3f79821b2280a55
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-02-23 19:31:15 +0530

    Sync adapter removed.

--- Planner.Droid/Extensions/AppConstants.cs
+++ Planner.Droid/Extensions/AppConstants.cs
@@ -2,8 +2,5 @@
 {
     public static class AppConstants
     {
-        public const string SYNC_ADAPTER_AUTHORITY = "Planner.Droid.Planner.Droid.datasync.provider";
-        public const string SYNC_ADAPTER_ACCOUNT_TYPE = "Planner.Droid.Planner.Droid.datasync.account";
-        public const string SYNC_ADAPTER_ACCOUNT = "dummyaccount";
     }
 }
