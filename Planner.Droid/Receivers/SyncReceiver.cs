a23156f392a5c3c1aac7a9186c5ea7ba04c37b0a
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-04-14 10:58:47 +0530

    Sync Receiver added.

---
+++ Planner.Droid/Receivers/SyncReceiver.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Net;
+using Planner.Droid.Services;
+
+namespace Planner.Droid.Receivers
+{
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { ConnectivityManager.ConnectivityAction })]
+    public class SyncReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {   
+            if (IsOnline(context))
+            {
+                _ = SyncService.Instance.SyncAsync(context);
+            }
+        }
+
+        public bool IsOnline(Context context)
+        {
+            var cm = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
+            return cm.ActiveNetworkInfo == null ? false : cm.ActiveNetworkInfo.IsConnected;
+        }
+    }
+}
