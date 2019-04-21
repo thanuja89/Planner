fbd5c52ed09f28d95e53a5f1d597ce9a67bdf20a
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-04-21 16:47:40 +0530

    Refactored.

--- Planner.Dto/SignUpResultDto.cs
+++ Planner.Dto/SignUpResultDto.cs
@@ -2,6 +2,12 @@
 {
     public class SignUpResultDTO
     {
+        public static SignUpResultDTO Success(string userId)
+            => new SignUpResultDTO() { UserId = userId, Succeeded = true };
+
+        public static SignUpResultDTO Failed(SignUpErrorType errorType)
+            => new SignUpResultDTO() { ErrorType = errorType, Succeeded = false };
+
         public string UserId { get; set; }
         public bool Succeeded { get; set; }
         public SignUpErrorType ErrorType { get; set; }
