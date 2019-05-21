67c28c8890359f94cd0772cb584e2c5f14691e91
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-05-21 10:22:25 +0530

    Delete user for existing user not confirmed email address when registering.

--- Planner.Domain/Entities/ApplicationUser.cs
+++ Planner.Domain/Entities/ApplicationUser.cs
@@ -6,5 +6,15 @@ namespace Planner.Domain.Entities
     public class ApplicationUser : IdentityUser
     {
         public virtual ICollection<ScheduledTask>SheduledTasks { get; set; }
+
+        public static ApplicationUser NewExternalUser(string email)
+        {
+            return new ApplicationUser()
+            {
+                UserName = email,
+                Email = email,
+                EmailConfirmed = true
+            };
+        }
     }
 }
