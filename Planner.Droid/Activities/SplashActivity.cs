2de4b06d89bfd0fd5f8060bc36807b6ca83b6f26
Thanuja Dilhan <Thanujadilhan@gmail.com>
2019-05-28 15:19:42 +0530

    Main Activity removed.

--- Planner.Droid/Activities/SplashActivity.cs
+++ Planner.Droid/Activities/SplashActivity.cs
@@ -1,7 +1,13 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
+using Android.Runtime;
+using Android.Util;
 using Android.Views.Animations;
 using Android.Widget;
+using Planner.Droid.Services;
+using Planner.Mobile.Core;
+using Planner.Mobile.Core.Helpers;
 using System;
 using System.Threading.Tasks;
 
@@ -32,6 +38,8 @@ namespace Planner.Droid.Activities
 
         private async void StartAnimation()
         {
+            var initTask = InitAsync();
+
             imageView = FindViewById<ImageView>(Resource.Id.splash_ImageView);
 
             var set = new AnimationSet(true)
@@ -41,15 +49,68 @@ namespace Planner.Droid.Activities
                 FillEnabled = true
             };
 
-            set.AddAnimation(new RotateAnimation(0f, 360f, Dimension.RelativeToSelf, 0.5f, Dimension.RelativeToSelf, 0.50f));
+            set.AddAnimation(
+                new RotateAnimation(0f, 360f, Dimension.RelativeToSelf, 0.5f, Dimension.RelativeToSelf, 0.50f));
 
-            set.AddAnimation(new ScaleAnimation(1f, 3f, 1f, 3f, Dimension.RelativeToSelf, 0.5f, Dimension.RelativeToSelf, 0.5f));
+            set.AddAnimation(
+                new ScaleAnimation(1f, 3f, 1f, 3f, Dimension.RelativeToSelf, 0.5f, Dimension.RelativeToSelf, 0.5f));
 
             imageView.StartAnimation(set);
 
-            await Task.Delay(TimeSpan.FromSeconds(5));
+            await Task.WhenAll(initTask, Task.Delay(TimeSpan.FromSeconds(5)));
 
-            StartActivity(typeof(MainActivity));         
+            if (initTask.Result) // Task is already complete, so we won't block the thread
+                StartActivity(typeof(TasksActivity));
+            else
+                StartActivity(typeof(SignInActivity));        
+        }
+
+        private Task<bool> InitAsync()
+        {
+            return Task.Run(async () =>
+            {
+                try
+                {
+                    AppDomain.CurrentDomain.UnhandledException -= CurrentDomain_UnhandledException;
+                    AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                    AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironment_UnhandledExceptionRaiser;
+
+                    var prefs = Application.Context
+                        .GetSharedPreferences(PreferenceKeys.USER_INFO, FileCreationMode.Private);
+
+                    var token = prefs.GetString(PreferenceItemKeys.TOKEN, null);
+                    string deviceRegToken = Helpers.Utilities
+                        .GetStringFromPreferences(PreferenceItemKeys.FIREBASE_REG_TOKEN);
+
+                    if (token != null)
+                    {
+                        HttpHelper.Init(token, deviceRegToken);
+
+                        await SyncService.Instance.SyncAsync();
+
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                catch (Exception ex)
+                {
+                    Log.WriteLine(LogPriority.Error, "Planner Error", ex.Message);
+                    StartActivity(typeof(SignInActivity));
+
+                    return false;
+                }
+            });
+        }
+
+        private void AndroidEnvironment_UnhandledExceptionRaiser(object sender, RaiseThrowableEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
